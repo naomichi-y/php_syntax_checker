7ce4586eec0893c926122493981d3b81c0a4d21e
Naomichi Yamakita <yamakita@dtx.co.jp>
2012-08-31 18:19:31 +0900

    -

--- PhpSyntaxChecker.py
+++ PhpSyntaxChecker.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, os, commands, re
+import sublime, sublime_plugin, os, commands, re, subprocess, sys
 
 class PhpSyntaxChecker(sublime_plugin.EventListener):
   # Command refers to $PATH environment variable
@@ -14,17 +14,30 @@ class PhpSyntaxChecker(sublime_plugin.EventListener):
 
     if extension in self.TARGET_SUFFIXES:
       command = self.EXECUTE_COMMAND + " " + path
-      response = commands.getoutput(command)
+      proc = subprocess.Popen([command],
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
 
-      r = re.compile("Parse error.* on line (\d+)")
-      match = r.search(response)
+      response = proc.communicate()
+      stdout = response[0]
+      stderr = response[1]
 
-      if match != None:
-        line = int(match.group(1)) - 1
+      if len(stderr):
+        sublime.error_message("Execute error\n" + stderr)
+ 
+      else:
+        rc = re.compile("Parse error.* on line (\d+)")
+        match = rc.search(stdout)
 
-        offset = view.text_point(line, 0)
-        view.sel().clear()
-        view.sel().add(sublime.Region(offset))
-        view.show(offset)
+        if match != None:
+          sublime.error_message("PHP Syntax error\n" + stdout)
 
-        sublime.error_message("PHP Syntax error\n" + response)
+          line = int(match.group(1)) - 1
+          offset = view.text_point(line, 0)
+
+          sel = view.sel()
+          sel.clear()
+          sel.add(sublime.Region(offset))
+
+          view.show(offset)

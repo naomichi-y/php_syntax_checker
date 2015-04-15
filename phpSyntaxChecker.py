d2748ff39230c54570f4000286c28d1352b9cb31
naomichi-y <n.yamakita@gmail.com>
2015-04-16 05:32:04 +0900

    fixed #2

--- phpSyntaxChecker.py
+++ phpSyntaxChecker.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin
-import os, re, subprocess
+import re, sys, subprocess, shlex
 
 class phpSyntaxCheckerCommand(sublime_plugin.EventListener):
   # Command refers to $PATH environment variable
@@ -14,24 +14,34 @@ class phpSyntaxCheckerCommand(sublime_plugin.EventListener):
     root, extension = os.path.splitext(path)
 
     if extension in self.TARGET_SUFFIXES:
-      command = self.EXECUTE_COMMAND + " \"" + path + "\""
-      proc = subprocess.Popen([command],
-        shell = True,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE)
+      command = self.EXECUTE_COMMAND + " " + path
+      args = shlex.split(command, False, False)
+      shell = True
 
-      response = proc.communicate()
-
-      if int(sublime.version()) > 3000:
-        stdout = self.sanitize(str(response[0]))
-        stderr = self.sanitize(str(response[1]))
-
+      if sublime.platform() == "windows":
+        encoding = "sjis"
       else:
-        stdout = str(response[0])
-        stderr = str(response[1])
+        encoding = "utf-8"
+
+        if int(sublime.version()) >= 3000:
+          shell = False
+
+      if int(sublime.version()) >= 3000:
+        params = args
+      else:
+        params = [command]
+
+      response = subprocess.Popen(
+        params,
+        shell=shell,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE).communicate()
+
+      stdout = response[0].decode(encoding)
+      stderr = response[1].decode(encoding)
 
       if len(stderr):
-        sublime.error_message("Execute error:\n" + stderr)
+        sublime.error_message(stderr)
 
       else:
         rc = re.compile("Parse error.* on line (\d+)")
@@ -48,11 +58,4 @@ class phpSyntaxCheckerCommand(sublime_plugin.EventListener):
           sel.add(sublime.Region(offset))
 
           view.show(offset)
-
-  def sanitize(self, data):
-    data = re.sub(r'^b[\'"]', '', data)
-    data = re.sub(r'[\'"]$', '', data)
-    data = re.sub(r'\\n', "\n", data)
-
-    return data
 
